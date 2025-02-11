575a5dd96b7144d38d158d5ed43cee7dcd0b6dfd
cto-deak-ai <cto@deak.ai>
2025-02-11 09:08:01 +0100

    testing additional custom settings

--- plugins/withCustomXcodeSettings.ts
+++ plugins/withCustomXcodeSettings.ts
@@ -7,8 +7,16 @@ const withCustomXcodeSettings: ConfigPlugin = (config) => {
     // Locate all build configurations
     Object.values(project.pbxXCBuildConfigurationSection()).forEach((buildConfig: any) => {
       if (typeof buildConfig.buildSettings !== 'undefined') {
-        // Add or modify the build setting
+        // Add or modify the build settings
         buildConfig.buildSettings.ENABLE_USER_SCRIPT_SANDBOXING = 'NO';
+        
+        // Add settings for react-native-fast-encoder
+        if (buildConfig.buildSettings.PRODUCT_NAME === 'react-native-fast-encoder') {
+          buildConfig.buildSettings.CLANG_CXX_LANGUAGE_STANDARD = 'gnu++17';
+          buildConfig.buildSettings.IPHONEOS_DEPLOYMENT_TARGET = '13.0';
+          buildConfig.buildSettings.EXCLUDED_ARCHS = '';
+          buildConfig.buildSettings.ENABLE_BITCODE = 'NO';
+        }
       }
     });
 
